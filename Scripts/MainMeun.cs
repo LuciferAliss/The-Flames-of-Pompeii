39331d9b1ca37e8ba8aa1af07197cededd43fd93
Vladislav <vladislav.a.koltovicha@gmail.com>
2024-10-06 22:22:30 +0300

    Допил настроек и сохранение
    Сделана вся ветка настроек связанная с экраном, сделана система сохранения настроек в текстовый файл

--- Scripts/MainMeun.cs
+++ Scripts/MainMeun.cs
@@ -1,16 +1,54 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class MainMeun : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		LoadSettingsGame(GetDictionary());
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private Dictionary<string, string> GetDictionary()
+	{
+		Dictionary<string, string> dictionary = new Dictionary<string, string>();
+		var Settings = FileAccess.Open("res://Save//SaveSettngsGame.txt", FileAccess.ModeFlags.Read);
+
+        while (!Settings.EofReached())
+        {
+            string line = Settings.GetLine();
+            var parts = line.Split(new[] { '=' }, 2);
+
+            if (parts.Length == 2)
+            {
+                string key = parts[0].Trim();
+                string value = parts[1].Trim();
+                dictionary[key] = value;
+            }
+        }
+		Settings.Close();
+		return dictionary;
+	}
+
+	private void LoadSettingsGame(Dictionary<string, string> Settings)
+	{
+		Vector2I PermissionSize;
+		List<string> Permission = Settings["Permission"].Split("x").ToList();
+ 		PermissionSize = new Vector2I(int.Parse(Permission[0]), int.Parse(Permission[1]));
+		DisplayServer.WindowSetSize(PermissionSize);
+		
+		if (bool.Parse(Settings["Fullscreen"])) DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+		else DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+
+		float GlobalBrightnessValue = float.Parse(Settings["GlobalBrightness"]);
+		var GlobalBrightness = GetNode<WorldEnvironment>("GlobalBrightness");
+		GlobalBrightness.Environment.AdjustmentBrightness = GlobalBrightnessValue;
 	}
 
 	private void ChangeSceneToSettings()
--- Scripts/Settings.cs
+++ Scripts/Settings.cs
@@ -1,14 +1,15 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class Settings : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		LoadSettingsGame(GetDictionary());
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
@@ -23,4 +24,128 @@ public partial class Settings : Node2D
 		GetTree().CurrentScene = nextSceneInstance;
 		currentScene.QueueFree();
 	}
+
+	private void PermissionSelected(int id)
+	{
+		Vector2I PermissionSize = new Vector2I(1920, 1080);;
+		switch (id)
+		{
+			case 0:
+				PermissionSize = new Vector2I(1920, 1080);
+				DisplayServer.WindowSetSize(PermissionSize);
+				break;	
+			case 1:
+				PermissionSize = new Vector2I(1600, 900);
+				DisplayServer.WindowSetSize(PermissionSize);
+				break;	
+			case 2:
+				PermissionSize = new Vector2I(1280, 720);
+				DisplayServer.WindowSetSize(PermissionSize);
+				break;	
+		}
+		string Permission = $"{PermissionSize.X.ToString()}x{PermissionSize.Y.ToString()}";
+		SaveSettingsGame(GetDictionary(), "Permission", Permission);
+	}
+	private void UseFullScreenMode()
+	{
+		bool FullScreenModePosition = bool.Parse(GetDictionary()["Fullscreen"]);
+		var FullScreenModeButton = GetNode<TextureButton>("BackgroundSettings/Screen/FullScreenModeButton");
+		if(FullScreenModePosition)
+		{
+			FullScreenModePosition = false;
+			FullScreenModeButton.TextureNormal = (Texture2D)GD.Load("res://Images/Settings/CheckBoxFalse.png");
+			DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+		}
+		else 
+		{
+			FullScreenModePosition = true;
+			FullScreenModeButton.TextureNormal = (Texture2D)GD.Load("res://Images/Settings/CheckBoxTrue.png");
+			DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+		}
+		SaveSettingsGame(GetDictionary(), "Fullscreen", FullScreenModePosition.ToString());
+		LoadSettingsGame(GetDictionary());
+	}
+
+	private void ChangeBrightness(float brightness)
+	{
+		var GlobalBrightness = GetNode<WorldEnvironment>("GlobalBrightness");
+		GlobalBrightness.Environment.AdjustmentBrightness = brightness;
+		SaveSettingsGame(GetDictionary(), "GlobalBrightness", brightness.ToString());
+	}
+
+	private Dictionary<string, string> GetDictionary()
+	{
+		Dictionary<string, string> dictionary = new Dictionary<string, string>();
+		var Settings = FileAccess.Open("res://Save//SaveSettngsGame.txt", FileAccess.ModeFlags.Read);
+
+        while (!Settings.EofReached())
+        {
+            string line = Settings.GetLine();
+            var parts = line.Split(new[] { '=' }, 2);
+
+            if (parts.Length == 2)
+            {
+                string key = parts[0].Trim();
+                string value = parts[1].Trim();
+                dictionary[key] = value;
+            }
+        }
+		Settings.Close();
+		return dictionary;
+	}
+
+	private void LoadSettingsGame(Dictionary<string, string> Settings)
+	{
+		var PermissionSelectedButton = GetNode<OptionButton>("BackgroundSettings/Screen/PermissionSelectedButton");
+		switch (int.Parse(Settings["Permission"].Split("x").ToArray()[0]))
+		{
+			case 1920:
+				PermissionSelectedButton.Select(0);
+				PermissionSelected(0);
+				break;
+			case 1600:
+				PermissionSelectedButton.Select(1);
+				PermissionSelected(1);
+				break;
+			case 1280:
+				PermissionSelectedButton.Select(2);
+				PermissionSelected(2);
+				break;
+		}
+		
+		bool FullScreenModePosition = bool.Parse(Settings["Fullscreen"]);
+		var FullScreenModeButton = GetNode<TextureButton>("BackgroundSettings/Screen/FullScreenModeButton");
+		if (FullScreenModePosition) FullScreenModeButton.TextureNormal = (Texture2D)GD.Load("res://Images/Settings/CheckBoxTrue.png");
+		else FullScreenModeButton.TextureNormal = (Texture2D)GD.Load("res://Images/Settings/CheckBoxFalse.png");
+
+		float GlobalBrightnessValue = float.Parse(Settings["GlobalBrightness"]);
+		var GlobalBrightness = GetNode<WorldEnvironment>("GlobalBrightness");
+		var BrightnessScrollBar = GetNode<HScrollBar>("BackgroundSettings/Screen/BrightnessScrollBar");
+		GlobalBrightness.Environment.AdjustmentBrightness = GlobalBrightnessValue;
+		BrightnessScrollBar.Value = GlobalBrightnessValue;
+	}
+	
+	private void SaveSettingsGame(Dictionary<string, string> Settings, string NameSetting, string ValueSetting)
+	{
+		string NewSettings = ""; 
+		foreach (var Setting in Settings)
+		{
+			if (Setting.Key == NameSetting)
+			{
+				string key = Setting.Key;
+				string value = ValueSetting;
+				NewSettings += $"{key}={value}\n";
+			}
+			else
+			{
+				string key = Setting.Key;
+				string value = Setting.Value;
+				NewSettings += $"{key}={value}\n";
+			}
+		}
+		NewSettings = NewSettings.TrimEnd('\n');
+        var settingsFile = FileAccess.Open("res://Save//SaveSettngsGame.txt", FileAccess.ModeFlags.Write);
+        settingsFile.StoreString(NewSettings);
+        settingsFile.Close();
+	} 
 }
