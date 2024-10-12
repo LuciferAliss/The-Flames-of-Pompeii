0af37cd5ee3969de68bcc4e12137b14538faf2b6
Vladislav <vladislav.a.koltovicha@gmail.com>
2024-10-12 09:43:03 +0300

    Доделка мобов
    Добавление доп анимаций

--- Scripts/Mobs/Skeleton.cs
+++ Scripts/Mobs/Skeleton.cs
@@ -4,16 +4,18 @@ using System;
 public partial class Skeleton : Mob, ObjectMove
 {
 	CharacterBody2D player;
+	private AnimatedSprite2D Animated;
 	bool chase = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Animated = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		player = PlayerCharacter.Instance;
-		Speed = 200;
+		Speed = 150;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
 		Move(delta);
 	}
@@ -32,10 +34,21 @@ public partial class Skeleton : Mob, ObjectMove
 		if (chase)
 		{
 			velocity.X = Direction.X * Speed;
+			Animated.Play("Run");
  		}
 		else 
 		{
 			velocity.X = 0;	
+			Animated.Play("Idle");
+		}
+		
+		if(Direction.X < 0)
+		{
+			Animated.FlipH = true;
+		} 
+		else
+		{
+			Animated.FlipH = false;
 		}
 
 		Velocity = velocity;
